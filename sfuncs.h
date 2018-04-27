da9e5bdf6d5cc030e9f6a8cc2ad812f1ed03b6a3
Deadly Headshot <ddlyheadshot@gmail.com>
2018-04-27 14:49:47 +0100

    Revert "Added comments to string functions"
    This reverts commit e4d0bf2358ada39b943e16df1450c90274aa87d8.

--- sfuncs.h
+++ sfuncs.h
@@ -1,19 +1,19 @@
 #ifndef __INC_SFUNCS_H__
 #define __INC_SFUNCS_H__
 
-void strtoupper(char *outstr, char *instr); /* Uppercase */
-int streq_(char *a, char *b);  /* Strings equal */
-int streq_i(char *a, char *b); /* Case insensitive strings equal */
-int startsame_(char *a, char *b); 
+void strtoupper(char *outstr, char *instr);
+int streq_(char *a, char *b);
+int streq_i(char *a, char *b);
+int startsame_(char *a, char *b);
 int startsame_i(char *a, char *b);
-int endwith_(char *exstring, char *refstring);  /* Does exstring ends with refstring? */
-int endwith_i(char *exstring, char *refstring); /* Case insensitive: does exstring end with refstring? */
-void strltrim(char *ostr, char *istr);  /* Left trim off spaces */
-void strtrimws(char *ostr, char *istr); /* Trim standard whitespace */
-int strtrimws1(char *astr);             /* In-place trim standard whitespace */
-void freentsa(char **antsa);  /* Free a null-terminated string array */
-long searchbackch(char *astr, char findchar); /* Find last findchar in astr */
-void strdsqs(char *outstr, char *instr); /* Escapes "'"s as "''" */
-long strLen(char *a); /* A safer strlen */
+int endwith_(char *exstring, char *refstring);
+int endwith_i(char *exstring, char *refstring);
+void strltrim(char *ostr, char *istr);
+void strtrimws(char *ostr, char *istr);
+int strtrimws1(char *astr);
+void freentsa(char **antsa);
+long searchbackch(char *astr, char findchar);
+void strdsqs(char *outstr, char *instr);
+long strLen(char *a);
 
 #endif
