5825f9cd26ed6615ca1747fbf2d0b189b65eff75
Deadly Headshot <ddlyheadshot@gmail.com>
2018-05-10 23:53:16 +0100

    Worked on command outline

--- main.h
+++ main.h
@@ -45,6 +45,43 @@ int docmd(char **cmdntsa, int offset);
  * help list item
  * help help
  */
+
+/*
+ * TODO: Commands:
+ * ---------------
+ * update channel (chanid) download none
+ * update channel (chanid) download latest
+ * update channel (chanid) download new
+ * update channel (chanid) download all
+ * download channel (chanid) none
+ * download channel (chanid) latest
+ * download channel (chanid) all
+ * download channel (chanid) item (itemid)
+ * play channel (chanid) none
+ * play channel (chanid) latest
+ * play channel (chanid) all
+ * play channel (chanid) item (itemid)
+ * help update
+ * help update channel
+ * help update channel download
+ * help update channel download none
+ * help update channel download latest
+ * help update channel download new
+ * help update channel download all
+ * help update channel download item
+ * help download
+ * help download channel
+ * help download channel none
+ * help download channel latest
+ * help download channel all
+ * help download channel item
+ * help play
+ * help play channel
+ * help play channel none
+ * help play channel latest
+ * help play channel all
+ * help play channel item
+ */
 int main(int argc, char *argv[]);
 int testparser(char *rssfile);
 int testdb();
